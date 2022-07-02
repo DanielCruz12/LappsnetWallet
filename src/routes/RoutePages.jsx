ef695f012b407f088b60641351e96eb3b47f0b22
daniel <danieldubonxd@gmail.com>
2022-07-01 23:02:31 -0600

    routes

--- src/routes/RoutePages.jsx
+++ src/routes/RoutePages.jsx
@@ -3,14 +3,25 @@ import { Routes, Route} from "react-router-dom";
 import Docs from "../pages/Docs";
 import Home from '../pages/Home';
 import Wallet from '../pages/Wallet';
+import Redeem from '../pages/Redeem';
 
 const RoutePages = () => {
   return (
     <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/wallet" element={<Wallet/>} />
       <Route path="/docs" element={<Docs/>} />
+
+{/*       router.get('/auth', function(req, res, next) {
+            // Note: this redirect will not work if port must be specified (e.g. localhost:3000)
+            let URL = require('url').URL
+            let newUrl = new URL(`${req.protocol}://${req.hostname}${req.originalUrl}`)
+            newUrl.pathname = 'wallet'
+            res.redirect(newUrl.href)
+}); */}
+
+      <Route path="/wallet" element={<Wallet />}/>
+      <Route path='/redeem' element={<Redeem/>} />
     </Routes>
   </>
   )
